74b16f89c3c62758b037a608b1fd254d899b5dcf
Nigel Archer <nigel@ic-hosting.com.au>
2019-06-21 00:55:01 +1000

    Major refactor
    Use blockchain as bitpay is broken/dodgy
    
    Simplify Code a lot
    
    Use async
    
    Allow for confirmation events

--- index.js
+++ index.js
@@ -1,110 +1,87 @@
-const io = require('socket.io-client')
-const debug = require('debug')('blt')
 const txdebug = require('debug')('blt:tx')
 const hashdebug = require('debug')('blt:hash')
 const blockdebug = require('debug')('blt:blockchain')
-const request = require('axios')
+
+var Socket = require('blockchain.info/Socket')
 
 const EventEmitter = require('events').EventEmitter
 
 module.exports = (config) => {
+  const blockexplorer = require('blockchain.info/blockexplorer').usingNetwork(config.testnet ? 3 : 0)
   const self = this
-  if (config === undefined) {
-    self.insight_servers = ['https://insight.bitpay.com/', 'https://www.localbitcoinschain.com/', 'https://search.bitaccess.co/']
-    self.insight_apis_servers = ['https://insight.bitpay.com/api/', 'https://www.localbitcoinschain.com/api/', 'https://search.bitaccess.co/insight-api/']
-  } else {
-    if (config.testnet === true) {
-      self.insight_servers = ['https://test-insight.bitpay.com/']
-      self.insight_apis_servers = ['https://test-insight.bitpay.com/api/']
-    }
-  }
   self.connected = false
   self.events = new EventEmitter()
-  self.getTxs = async (address) => (await self.getAddress(address)).txs
-  self.getBalance = async (address) => (await self.getAddress(address)).balance
+  self.getTxs = async (address) => (await blockexplorer.getAddress(address)).txs
+  self.getBalance = async (address) => (await blockexplorer.getAddress(address)).final_balance
   self.getAddress = async (address) => {
     const result = {}
     blockdebug('Getting txs for address', address, 'url:', self.api_url + 'txs/?address=' + address)
-
     result.address = address
     result.in = 0
     result.out = 0
     result.curr = 'bits(uBTC)'
-    const response = await axios(self.api_url + 'txs/?address=' + address)
-    blockdebug('success :)')
+    const response = await blockexplorer.getAddress(address)
     console.log(response)
-    var transaction_json = response.data
-    transaction_json.txs.forEach(each_tx => {
-      each_tx.vout.forEach(each_vout => {
-        each_vout.scriptPubKey.addresses.forEach(outaddress => {
-          // console.log('checking', outaddress)
+    response.txs.forEach(eachTx => {
+      eachTx.out.forEach(eachOut => {
+        eachOut.scriptPubKey.addresses.forEach(outaddress => {
           if (outaddress === address) {
-            // console.log('adding!', each_vout.value)
-            result.in = result.in + each_vout.value * 1000000
+            result.in = result.in + eachOut.value
           }
         })
       })
-      each_tx.vin.forEach(each_vin => {
-        // each_vin.scriptPubKey.addresses.forEach(function(outaddress) {
-        // console.log('checking', outaddress)
-        if (each_vin.addr === address) {
-          // console.log('adding!', each_vout.value)
-          result.out = result.out + each_vin.value * 1000000
+      eachTx.inputs.forEach(eachIn => {
+        if (eachIn.addr === address) {
+          result.out = result.out + eachIn.value
         }
-        // })
       })
     })
     result.balance = result.in - result.out
-    result.txs = transaction_json.txs.length
-    return { txs: response.data, balance: result }
+    result.txs = response.txs.length
   }
-
+  self.socket = new Socket({ network: config.testnet ? 3 : 0 })
   self.connect = async () => {
-    return new Promise((resolve, reject) => {
-      if (self.connected === false) {
-        self.url = self.insight_servers.shift()
-        self.api_url = self.insight_apis_servers.shift()
-        if (self.url !== undefined) {
-          self.socket = io(self.url)
-          setTimeout(() => {
-            if (self.connected === false) {
-              debug('Could not connect, trying again...')
-              self.socket.disconnect()
-              self.connect()
-            }
-          }, 5000)
-
-          self.socket.on('connect', function () {
-            self.connected = true
-            self.socket.emit('subscribe', 'inv')
-            self.events.emit('connected')
-            resolve()
-          })
-          self.socket.on('tx', function (data) {
-            self.events.emit('tx', data)
-            data.vout.forEach(eachVout => {
-              hashdebug({ address: Object.keys(eachVout)[0], amount: eachVout[Object.keys(eachVout)[0]] })
-              self.events.emit(Object.keys(eachVout)[0], { address: Object.keys(eachVout)[0], amount: eachVout[Object.keys(eachVout)[0]] })
-            })
-            txdebug('New transaction received: ' + JSON.stringify(data))
-          })
-          self.socket.on('block', function (data) {
-            self.events.emit('block', data)
-            blockdebug('New block received: ' + JSON.stringify(data))
-          })
-          self.socket.on('event', function (data) {
-            debug('event', data)
-          })
-          self.socket.on('disconnect', function (d) {
-            debug('disconnect!', d)
-          })
-          self.socket.on('error', function (e) {
-            debug('error!', e)
-          })
-        } else {
-          reject('Cannot reach any bitcoin insight server... no bitcoin transactions are being received.')
+    self.socket.onOpen(() => {
+      self.connected = true
+      self.socket.emit('subscribe', 'inv')
+      self.events.emit('connected')
+    })
+    self.watchingAddresses = []
+    self.watchAddress = address => {
+      self.watchingAddresses.push(address)
+    }
+    self.getTxConfirmation = async (tx, payment) => {
+      const loaded = await blockexplorer.getTx(tx.hash)
+      if (self.latestBlock) {
+        if (self.latestBlock.height - loaded.block_height > 6 && !loaded.double_spend) {
+          self.events.emit('confirmed', payment)
+          return
+        } else if (loaded.double_spend) {
+          self.events.emit('doubleSpend', payment)
+          return
         }
       }
+      // Still not confirm.. call back in 12 minutes
+      setTimeout(self.getTxConfirmation, 12 * 60 * 1000, tx, payment)
+    }
+    self.socket.onTransaction((data) => {
+      self.events.emit('tx', data)
+      data.out.forEach(eachVout => {
+        if (self.watchingAddresses.includes(eachVout.addr)) {
+          hashdebug({ hash: data.hash, address: eachVout.addr, amount: eachVout.value })
+          const payment = { hash: data.hash, address: eachVout.addr, amount: eachVout.value }
+          self.events.emit(eachVout.addr, payment)
+          // check for confirmations on the block in about 30 minutes (takes about 60 minutes to get full confirmation)
+          setTimeout(self.getTxConfirmation, 30 * 60 * 1000, data, payment)
+        }
+      })
+      txdebug('New transaction received: ' + JSON.stringify(data))
+    })
+    self.latestBlock = null
+    self.socket.onBlock((data) => {
+      self.latestBlock = data
+      self.events.emit('block', data)
+      blockdebug('New block received: ' + JSON.stringify(data))
     })
   }
   return self
